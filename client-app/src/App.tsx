9abda91bdba82438cea9fc08bd98ab2b4b547a03
Prabin.Lama <prabin.lama@sbl.com.np>
2023-07-05 17:41:34 +0545

    added semantic ui

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
+import { Header, List } from 'semantic-ui-react';
 
 function App() {
   const [activities, setActivities] = useState([]);
@@ -9,22 +10,22 @@ function App() {
   useEffect(() => {
     axios.get('http://localhost:5000/api/activities')
       .then(response => {
-          console.log(response);
+          //console.log(response);
           setActivities(response.data);
       })
   }, [])
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+      <Header as='h2' icon='users' content='Reactivities' />
 
-       <ul>
+       <List>
           {activities.map((activity: any) => (
-              <li key={activity.id}>{activity.title}</li>
+              <List.Item key={activity.id}>
+                {activity.title}
+                </List.Item>
           ))}
-       </ul>
-      </header>
+       </List>
     </div>
   );
 }
--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'semantic-ui-css/semantic.min.css'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -9,9 +10,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <App />
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>
+ 
 );
 
 // If you want to start measuring performance in your app, pass a function
